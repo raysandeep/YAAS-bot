2e052e870cadb580e66f96530a8cb6fa753cb86e
Sai Sandeep Rayanuthala <43823311+unknown-guy-1610@users.noreply.github.com>
2019-07-12 03:05:41 +0530

    Update Details.py
    Updated few lines

--- Details.py
+++ Details.py
@@ -5,7 +5,14 @@ import urllib.error
 from bs4 import BeautifulSoup
 import ssl
 import json
-
+from pprint import pprint
+from random import choice
+import os
+from lxml import html
+from InstagramAPI import InstagramAPI
+_user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+]
 
 class Insta_Info_Scraper:
 
@@ -14,8 +21,8 @@ class Insta_Info_Scraper:
         soup = BeautifulSoup(html, 'html.parser')
         data = soup.find_all('meta', attrs={'property': 'og:description'
                              })
+        
         text = data[0].get('content').split()
-        print(text)
         self.user = ""
         for i in text:
             if i == "from":
@@ -24,20 +31,115 @@ class Insta_Info_Scraper:
         followers = text[0]
         following = text[2]
         self.posts = text[4]
-        print ('User:', self.user)
+        '''print ('User:', user)
         print ('Followers:', followers)
         print ('Following:', following)
-        #print ('Posts:', posts)
-        print ('---------------------------')
-
-    def main(self):
+        print ('Posts:', posts)
+        print ('---------------------------')'''
+    def main(self,username):
         self.ctx = ssl.create_default_context()
         self.ctx.check_hostname = False
         self.ctx.verify_mode = ssl.CERT_NONE
-        url1 ='https://www.instagram.com/vit_bot/'
+        url1 = "https://www.instagram.com/"+ username.split("@")[1].split(")")[0] + "/"
         self.getinfo(url1)
-        return self.posts
+        return self.user,self.posts
 
+
+
+class InstagramScraper:
+
+    def __init__(self, user_agents=None, proxy=None):
+        self.user_agents = user_agents
+        self.proxy = proxy
+
+    def __random_agent(self):
+        if self.user_agents and isinstance(self.user_agents, list):
+            return choice(self.user_agents)
+        return choice(_user_agents)
+
+    def __request_url(self, url):
+        try:
+            response = requests.get(url, headers={'User-Agent': self.__random_agent()}, proxies={'http': self.proxy,
+                                                                                                 'https': self.proxy})
+            response.raise_for_status()
+        except requests.HTTPError:
+            raise requests.HTTPError('Received non 200 status code from Instagram')
+        except requests.RequestException:
+            raise requests.RequestException
+        else:
+            return response.text
+
+    @staticmethod
+    def extract_json_data(html):
+        soup = BeautifulSoup(html, 'html.parser')
+        body = soup.find('body')
+        script_tag = body.find('script')
+        raw_string = script_tag.text.strip().replace('window._sharedData =', '').replace(';', '')
+        return json.loads(raw_string)
+
+    def profile_page_metrics(self, profile_url):
+        results = {}
+        try:
+            response = self.__request_url(profile_url)
+            json_data = self.extract_json_data(response)
+            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']
+        except Exception as e:
+            raise e
+        else:
+            for key, value in metrics.items():
+                if key != 'edge_owner_to_timeline_media':
+                    if value and isinstance(value, dict):
+                        value = value['count']
+                        results[key] = value
+                    elif value:
+                        results[key] = value
+        return results
+
+    def profile_page_recent_posts(self, profile_url):
+        results = []
+        try:
+            response = self.__request_url(profile_url)
+            json_data = self.extract_json_data(response)
+            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']["edges"]
+        except Exception as e:
+            raise e
+        else:
+            for node in metrics:
+                node = node.get('node')
+                if node and isinstance(node, dict):
+                    results.append(node)
+        return results
+
+    
 if __name__ == '__main__':
-    obj = Insta_Info_Scraper()
-    obj.main()
+    InstagramAPI = InstagramAPI("vit_bot", "yaas123")
+    InstagramAPI.login()    
+    bot =	{
+            " VIT Bot (@vit_bot)" : 1 ,
+            " Anand (@__anandsure)" : 62 ,
+            " Sandeep (@cant_find_new_name)" : 7
+            }
+    while(True):
+        for x in bot:
+            us,po = Insta_Info_Scraper().main(x)
+            print(us,po,bot[us])
+            if int(po)>bot[us]:
+                k = InstagramScraper()
+                namee = us.split("@")[1].split(")")[0]
+                u = 'https://www.instagram.com/' + namee  +'/?hl=en'
+                results = k.profile_page_recent_posts(u)
+                for i in range(int(po)-bot[us]):
+                    url = results[-i]['display_url']
+                    cap = "Repost from : " + us + results[i-1]['edge_media_to_caption']['edges'][0]['node']['text']
+                    urllib.request.urlretrieve(url,"test.jpg")
+                  # login
+                    photo_path = 'test.jpg'
+                    InstagramAPI.uploadPhoto(photo_path, caption=cap)
+                    os.remove("test.jpg")
+                    print(i)
+                    bot[us]+=1
+                    print(bot[us])
+                    break
+            elif int(po)< bot[us] :
+                print(bot[us])
+                bot[us] = int(po)
