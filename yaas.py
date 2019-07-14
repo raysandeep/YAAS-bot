49f0fbf102d831b091ffa6360e43d6191622e870
Akshat Gupta <33231334+akshatvg@users.noreply.github.com>
2019-07-14 18:11:56 +0530

    final updated

--- yaas.py
+++ yaas.py
@@ -9,14 +9,13 @@ from random import choice
 import os
 from lxml import html
 from InstagramAPI import InstagramAPI
+import pandas as pd 
+import csv
 import time
 import subprocess
 _user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
 ]
-import os
-import sys
-import logging
 def run_again(cmd):
     subprocess.call(["bash", "-c", "source ~/.profile; " + cmd]) 
 
@@ -37,16 +36,18 @@ class Insta_Info_Scraper:
         followers = text[0]
         following = text[2]
         self.posts = text[4]
-        '''print ('User:', user)
+        '''print ('User:', self.user)
         print ('Followers:', followers)
         print ('Following:', following)
-        print ('Posts:', posts)
+        print ('Posts:', self.posts)
         print ('---------------------------')'''
     def main(self,username):
         self.ctx = ssl.create_default_context()
         self.ctx.check_hostname = False
         self.ctx.verify_mode = ssl.CERT_NONE
+        #print(username)
         url1 = "https://www.instagram.com/"+ username.split("@")[1].split(")")[0] + "/"
+        #print(url1)
         self.getinfo(url1)
         return self.user,self.posts
 
@@ -116,10 +117,12 @@ class InstagramScraper:
 
 InstagramAPI2 = InstagramAPI("yaas_bot", "yaas123")
 InstagramAPI2.login()    
-bot =	{
-        " Bot (@bot_trial123)" : 0 ,
-        " Anand (@__anandsure)" : 60
-        }
+dnj=[]
+pos = []
+with open(r"/Users/akshatvg/Desktop/insta-bot/works/final.csv") as csv_file:  
+    csv_reader = csv.reader(csv_file, delimiter=',') 
+    df = pd.DataFrame([csv_reader], index=None) 
+    df.head() 
 c=0
 f=0
 while(True and f==0):
@@ -127,37 +130,39 @@ while(True and f==0):
     while(True):
         print(c)
         c+=1
-        for x in bot:
-            us,po = Insta_Info_Scraper().main(x)
-            print(us,po,bot[us])
-            time.sleep(2)
-            if int(po)>bot[us]:
-                k = InstagramScraper()
-                namee = us.split("@")[1].split(")")[0]
-                u = 'https://www.instagram.com/' + namee  +'/?hl=en'
-                results = k.profile_page_recent_posts(u)
-                for i in range(int(po)-bot[us]):
-                    print(i)
-                    url = results[0]['display_url']
-                    has = "\n\n.............\n#yaas #teamyaas #vitbot #vit #velloreinstitueoftechnology #technology "
-                    if results[0]['edge_media_to_caption']['edges']==[]:
-                        cap = "Repost from : " + us + " "
-                        cap += has
-                    else:
-                        cap = "Repost from : " + us + " "+ results[0]['edge_media_to_caption']['edges'][0]['node']['text']
-                        cap += has
+        for x in range(1,43):
+            for val in df[x]:
+                #print(val[1])
+                us,po = Insta_Info_Scraper().main(val[1])
+                print(us,po,int(val[2]))
+                time.sleep(2)
+                if int(po)>int(val[2]):
+                    k = InstagramScraper()
+                    namee = us.split("@")[1].split(")")[0]
+                    u = 'https://www.instagram.com/' + namee  +'/?hl=en'
+                    results = k.profile_page_recent_posts(u)
+                    for i in range(int(po)-int(val[2])):
+                        print(i)
+                        url = results[0]['display_url']
+                        has = "#yaas #teamyaas #vitbot #vit #technology "
+                        if results[0]['edge_media_to_caption']['edges']==[]:
+                            cap = "Repost from : " + us + " "
+                            cap += has
+                        else:
+                            cap = "Repost from : " + us + " "+ results[0]['edge_media_to_caption']['edges'][0]['node']['text']
+                            cap += has
 
-                    urllib.request.urlretrieve(url,"test.jpg")
-                # login
-                    photo_path = 'test.jpg'
-                    InstagramAPI2.uploadPhoto(photo_path, caption=cap)
-                    os.remove("test.jpg")
-                    print(i)
-                    bot[us]+=1
-                    print(bot[us])
-                    break
-            elif int(po)< bot[us] :
-                print(bot[us])
-                bot[us] = int(po)
-    
-    
+                        urllib.request.urlretrieve(url,"test.jpg")
+                    # login
+                        photo_path = 'test.jpg'
+                        InstagramAPI2.uploadPhoto(photo_path, caption=cap)
+                        os.remove("test.jpg")
+                        print(i)
+                        df[val[2]] = int(df[val[2]])
+                        df[val[2]] += 1
+                        print(int(val[2]))
+                        break
+                elif int(po)< int(val[2]) :
+                    print(int(val[2]))
+                    df[val[2]] = po
+                    po = int(po)
